36395cc964fa03f29f4efcec2ad8d0798b03af2e
ignatramona22 <ignatramona22@stud.ase.ro>
2025-03-12 13:22:50 +0200

    calcul medie

--- Task1.c
+++ Task1.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<malloc.h>
-
+#include <stdlib.h>
+#include <string.h>
 //Ignat Ramona - Inventar
 
 struct Inventar {
@@ -14,31 +15,58 @@ struct Inventar citireInventar()
 {
 	struct Inventar inv;
 	printf("Introduceti codul: ");
-	scanf("%d", &inv.cod);
+	scanf_s("%d", &inv.cod);
 
 	char buffer[100];
 	printf("Introduceti denumirea produsului: ");
-	scanf(" %[^\n]", buffer);
+	scanf_s(" %[^\n]", buffer, (unsigned int)sizeof(buffer));
+
 	inv.denumireProdus = malloc(sizeof(char) * (strlen(buffer) + 1));
 	strcpy_s(inv.denumireProdus, (strlen(buffer) + 1), buffer);
 
 	printf("Introduceti numarul de produse: ");
-	scanf("%d", &inv.nrProduse);
+	scanf_s("%d", &inv.nrProduse);
 
 	inv.preturi = malloc(sizeof(float) * inv.nrProduse);
 	for (int i = 0; i < inv.nrProduse; i++)
 	{
-		printf("Introduceti pretul produsului %d", i + 1);
-		scanf("%f", &inv.preturi[i]);
+		printf("Introduceti pretul produsului %d: ", i + 1);
+		scanf_s("%f", &inv.preturi[i]);
 	}
 
 	return inv;
+}
+
+void afisareInventar(struct Inventar inv)
+{
+	printf("\nInventar:\n");
+	printf("Cod: %d\n", inv.cod);
+	printf("Denumire produs: %s\n", inv.denumireProdus);
+	printf("Numar produse: %d\n", inv.nrProduse);
+	printf("Preturi:");
+	for (int i = 0; i < inv.nrProduse; i++)
+	{
+		printf("%.2f ", inv.preturi[i]);
+	}
+	printf("\n");
+}
+
+float calculMediePreturi(struct Inventar inv)
+{
+	float suma = 0;
+	for (int i = 0; i < inv.nrProduse; i++)
+	{
+		suma += inv.preturi[i];
+	}
+	return suma / inv.nrProduse;
 }
 
 
 int main()
 {
 	struct Inventar inv = citireInventar();
+	afisareInventar(inv);
 
+	printf("\nMedia preturilor este: %.2f", calculMediePreturi(inv));
 	return 0;
 }
